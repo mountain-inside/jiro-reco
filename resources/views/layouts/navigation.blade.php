895db0f1095a3397b31156042b01c675bf7adf8d
mountain-inside <ce245034@meiji.ac.jp>
2024-09-17 19:45:43 +0900

    Breezeの画面表示の編集

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('tops')" :active="request()->routeIs('tops')">
+                        {{ __('Tops') }}
+                    </x-nav-link>
                 </div>
             </div>
 
