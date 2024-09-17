df36e9bdcd486f94ed02c3d8036157d2c0ba6ad9
mountain-inside <ce245034@meiji.ac.jp>
2024-09-17 19:29:30 +0900

    店舗情報にphoto_idの追加のデフォルト値の設定

--- database/migrations/2024_08_29_142033_create_stores_table.php
+++ database/migrations/2024_08_29_142033_create_stores_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->string('call_option', 100);
             $table->string('ticket_system', 50);
             $table->string('comment', 1000);
+            $table->string('photo_id')->default('https://res.cloudinary.com/duyuwjhzj/image/upload/v1726124206/cld-sample-4.jpg');
             $table->timestamps();
             $table->softDeletes();
         });
