e65d456334e2ea12268b433fda9fbce51a0431f3
mountain-inside <ce245034@meiji.ac.jp>
2024-09-21 20:15:46 +0900

    Breezeのレイアウトの設定

--- resources/views/reviews/edit.blade.php
+++ resources/views/reviews/edit.blade.php
@@ -7,22 +7,30 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body>
-        <h1 class="title">編集画面</h1>
-        <div class="content">
-            <form action="/reviews/{{ $review->id }}" method="POST">
-                @csrf
-                @method('PUT')
-                <div class='content__title'>
-                    <h2>ラーメン名</h2>
-                    <input type='text' name='review[foods]' value="{{ $review->foods }}">
-                </div>
-                <div class='content__body'>
-                    <h2>レビュー</h2>
-                    <input type='text' name='review[comment]' value="{{ $review->comment }}">
-                </div>
-                <input type="submit" value="保存">
-            </form>
-        </div>
-    </body>
+    <x-app-layout>
+        <x-slot name="header">
+        　レビュー編集
+        </x-slot>
+        <body>
+            <h1 class="title">編集画面</h1>
+            <div class="content">
+                <form action="/reviews/{{ $review->id }}" method="POST">
+                    @csrf
+                    @method('PUT')
+                    <div class='content__title'>
+                        <h2>ラーメン名</h2>
+                        <input type='text' name='review[foods]' value="{{ $review->foods }}">
+                    </div>
+                    <div class='content__body'>
+                        <h2>レビュー</h2>
+                        <input type='text' name='review[comment]' value="{{ $review->comment }}">
+                    </div>
+                    <input type="submit" value="保存">
+                </form>
+            </div>
+            <div class="footer">
+                <a href="/reviews/{{ $review->id }}">戻る</a>
+            </div>
+        </body>
+    </x-app-layout>
 </html>
--- resources/views/stores/index.blade.php
+++ resources/views/stores/index.blade.php
@@ -6,60 +6,65 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body>
-        <h1>ジロリアン.com</h1>
-        <h1>店舗一覧</h1>
-        <div class="search">
-            <form action="/stores" method="POST">
-                @csrf
-                <div class="form-group">
-                    <div>
-                        <label for="">スープの傾向
+    <x-app-layout>
+        <x-slot name="header">
+        　二郎店舗一覧
+        </x-slot>
+        <body>
+            <h1>ジロリアン.com</h1>
+            <h1>好みから二郎を選ぶ</h1>
+            <div class="search">
+                <form action="/search" method="POST">
+                    @csrf
+                    <div class="form-group">
                         <div>
-                            <select name="soup">
-                                <option value="">全て</option>
-                                @foreach ($soups as $soup)
-                                    <option value="{{ $soup->id }}">{{ $soup->soup }}</option>
-                                @endforeach
-                            </select>
+                            <label for="">スープの傾向
+                            <div>
+                                <select name="soup">
+                                    <option value="">全て</option>
+                                    @foreach ($soups as $soup)
+                                        <option value="{{ $soup->id }}">{{ $soup->soup }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            </label>
                         </div>
-                        </label>
-                    </div>
-
-                    <div>
-                        <label for="">麺の太さ
+    
                         <div>
-                            <select name="noodle">
-                                <option value="">全て</option>
-                                @foreach ($noodles as $noodle)
-                                    <option value="{{ $noodle->id }}">{{ $noodle->noodle }}</option>
-                                @endforeach
-                            </select>
+                            <label for="">麺の太さ
+                            <div>
+                                <select name="noodle">
+                                    <option value="">全て</option>
+                                    @foreach ($noodles as $noodle)
+                                        <option value="{{ $noodle->id }}">{{ $noodle->noodle }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            </label>
                         </div>
-                        </label>
+    
+                        <div>
+                            <input type="submit" class="btn" value="検索">
+                        </div>
                     </div>
-
-                    <div>
-                        <input type="submit" class="btn" value="検索">
+                </form>
+            </div>
+            <div class='stores'>
+                @foreach ($stores as $store)
+                    <div class='store'>
+                        <h2 class='name'>
+                            <a href="/stores/{{ $store->id }}">{{ $store->name }}</a>
+                        </h2>
+                        <p class='adress'>住所 {{ $store->adress}}</p>
+                        <p class='opening_hours'>営業時間 {{ $store->opening_hours }}</p>
+                        <p class='closed'>定休日{{ $store->closed }}</p>
+                        <p class='comment'>{{ $store->comment }}</p>
                     </div>
-                </div>
-            </form>
-        </div>
-        <div class='stores'>
-            @foreach ($stores as $store)
-                <div class='store'>
-                    <h2 class='name'>
-                        <a href="/stores/{{ $store->id }}">{{ $store->name }}</a>
-                    </h2>
-                    <p class='adress'>住所 {{ $store->adress}}</p>
-                    <p class='opening_hours'>営業時間 {{ $store->opening_hours }}</p>
-                    <p class='closed'>定休日{{ $store->closed }}</p>
-                    <p class='comment'>{{ $store->comment }}</p>
-                </div>
-            @endforeach
-        </div>
-        <div class='paginate'>
-            {{ $stores->links() }}
-        </div>
-    </body>
+                @endforeach
+            </div>
+            <div class='paginate'>
+                {{ $stores->links() }}
+            </div>
+        </body>
+    </x-app-layout>
 </html>
