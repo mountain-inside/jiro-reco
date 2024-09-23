2506c1076fe905b18c1ea00edd9e87a691a71fb5
mountain-inside <ce245034@meiji.ac.jp>
2024-09-23 16:51:56 +0900

    二郎店舗検索のための関数searchとpaginateの追加

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -25,4 +25,47 @@ class StoreController extends Controller
     {
         return view('stores.jiro')->with(['store' => $store]);
     }
+    
+    public function search(Store $store, Request $request, Soup $soup, Noodle $noodle)
+    {
+        $request_soup = $request->input('soup');
+        $request_noodle = $request->input('noodle');
+        $request->session()->put('request_soup', $request_soup);
+        $request->session()->put('request_noodle', $request_noodle);
+        $query = $store->newQuery();
+        
+        if(!empty($request_soup)) {
+            $query->where('soup_id', '=', $request_soup);
+        }
+
+        if(!empty($request_noodle)) {
+            $query->where('noodle_id', '=', $request_noodle);
+        }
+        
+        $stores=$query->paginate(5);
+        return view('stores.index')->with(['stores' => $stores, 'soups' => $soup->get(), 'noodles' => $noodle->get()]);
+    }
+    
+    public function paginate(Store $store, Request $request, Soup $soup, Noodle $noodle)
+    {
+        // セッションから検索条件を取得
+        $request_soup = $request->session()->get('request_soup');
+        $request_noodle = $request->session()->get('request_noodle');
+
+        // 条件分岐を行い、ページネーションを適用
+        $query = $store->newQuery();
+        
+        if(!empty($request_soup)) {
+            $query->where('soup_id', '=', $request_soup);
+        }
+
+        if(!empty($request_noodle)) {
+            $query->where('noodle_id', '=', $request_noodle);
+        }
+        
+        $stores=$query->paginate(5);
+
+        // 結果を返す
+        return view('stores.index', ['stores' => $stores, 'soups' => $soup->get(), 'noodles' => $noodle->get()]);
+    }
 }
