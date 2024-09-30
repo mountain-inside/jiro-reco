92443c8249420d3f6c0b132f465cc9739d73b899
mountain-inside <ce245034@meiji.ac.jp>
2024-09-30 15:46:03 +0900

    戻るボタンの追加

--- resources/views/reviews/show.blade.php
+++ resources/views/reviews/show.blade.php
@@ -33,9 +33,11 @@
                 </div>
             </div>
 
+            @if ($review->user_id == $user->id)
             <div class="edit text-center mt-8">
                 <a href="/reviews/{{ $review->id }}/edit" class="bg-yellow-500 text-black px-4 py-2 rounded shadow hover:bg-yellow-600">編集</a>
             </div>
+            @endif
 
             <div class="footer text-center mt-4">
                 <a href="/" class="text-yellow-500 hover:underline">戻る</a>
